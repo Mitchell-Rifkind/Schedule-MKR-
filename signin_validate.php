ff5575d416e9fc788c7a62f413547771b6005e93
Mitchell Rifkind <marifkind@gmail.com>
2017-05-02 11:12:54 -0400

    Start of the sign in validation. Will commect to the database and verify user

---
+++ signin_validate.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+
+      $email = $_POST["email"];
+      $password = $_POST["password"];
+
+
+
+     ?>
+  </body>
+</html>
