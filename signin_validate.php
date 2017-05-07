be3b9b48bd26382349057594c22e6f93ca44876b
Mitchell Rifkind <marifkind@gmail.com>
2017-05-07 14:25:39 -0400

    Removed and changes AWS keys that were accifdentally uploaded

--- signin_validate.php
+++ signin_validate.php
@@ -13,8 +13,7 @@
       $password = $_POST["password"];
 
       $client = DynamoDbClient::factory(array(
-        'key' => 'AKIAI6K6DFAL2EPNOT3A',
-        'secret' => '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'
+
         'version' => 'latest',
         'profile' => 'default',
         'region' => 'us-east-1'
