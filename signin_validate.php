7453459af7ce446e32b87265f9d62a93185850c0
Mitchell Rifkind <marifkind@gmail.com>
2017-05-09 15:17:40 -0400

    Set up the login to also have a new user section. When info is submitted new users will be added to the database, with a confirmation message appearing. If a login is attempted, the credentials will be assessed and a message will appear 'Granting Access' or 'Denying Access' based on validity. Checks will be added next to prevent copy emails from being used, new password verification, etc.

--- signin_validate.php
+++ signin_validate.php
@@ -10,34 +10,47 @@
       use Aws\DynamoDb\DynamoDbClient;
       use Aws\DynamoDb\Marshaler;
 
-      $email = $_POST["email"];
-      $password = $_POST["password"];
-
       $client = DynamoDbClient::factory(array(
         'version' => 'latest',
         'profile' => 'default',
         'region'  => 'us-east-1'
       ));
 
-      $result = $client->putItem(array(
-        'TableName' => 'User_Sign_In',
-        'Item' => array(
-          'email'    => array('S' => $email),
-          'password' => array('S' => $password)
-        )
-      ));
+      if (isset($_POST["email"]) && isset($_POST["password"])) {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-      $result = $client->getItem(array(
-        'ConsistentRead' => true,
-        'TableName' => 'User_Sign_In',
-        'Key'       => array(
-          'email'   => array('S' => $email)
-        )
-      ));
+        $result = $client->getItem(array(
+          'ConsistentRead' => true,
+          'TableName' => 'User_Sign_In',
+          'Key'       => array(
+            'email'     => array('S' => $email)
+            )
+        ));
 
-      echo "Welcome " . $result['Item']['email']['S'];
+        if ($password === $result['Item']['password']['S']) {
+          echo "Access Granted";
+        } else {
+          echo "Access Denied";
+        }
 
-      echo "\nInfo added successfully";
+      } else if (isset($_POST["email_new"]) && isset($_POST["password_new"])) {
+        $email_new = $_POST["email_new"];
+        $password_new = $_POST["password_new"];
+
+        $result = $client->putItem(array(
+          'TableName' => 'User_Sign_In',
+          'Item' => array(
+            'email'    => array('S' => $email_new),
+            'password' => array('S' => $password_new)
+          )
+        ));
+        echo "Welcome " . $email_new . ". Your account has been created";
+
+      } else {
+        echo "No information has been entered";
+      }
+
      ?>
   </body>
 </html>
