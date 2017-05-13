b63b951a4fb98b8eb15024efe5b6b450f8f53142
ilopez64 <stalop2009@gmail.com>
2017-05-12 21:00:19 -0400

    edits made to event-get.php

--- event-get.php
+++ event-get.php
@@ -6,4 +6,19 @@
     echo $_GET['details']; 
     handle_login($validation);
 ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <script type = "text/javascript" scr = "JS_Functions_Login.js"> </script>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
 
+        <script type="text/javascript"> // Needed in order to return to home.php or calendar-page.html with the proper values
+            toCalendar();
+        </script>
+    </body>
+</html>
